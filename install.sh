b3ecda7e19fc44a09379f4a996059065f5fde5e7
Falco <falco@falco.in>
2019-08-16 15:22:13 +0200

    fix pushd -> cd

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ cd falco-pub
 git clone https://github.com/falco-pub/vtiger-crm
 git clone https://github.com/falco-pub/mysql-utf8
 git clone https://github.com/falco-pub/vtiger
+docker build -t mysql-utf8:5.5 mysql-utf8/5.5/
+docker build -t vtiger-crm:7.1.0 vtiger-crm/
 cd vtiger
 docker-compose up
 
